03bfd0017a743f9da151af4022e2213206228f9b
Adison-tech <adisoncheruiyot55@gmail.com>
2026-07-16 17:37:29 +0300

    update

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,11 +1,11 @@
 // lib/db/index.ts
 import { drizzle } from 'drizzle-orm/d1';
-import { getRequestContext } from '@opennextjs/cloudflare';
+import { getCloudflareContext } from '@opennextjs/cloudflare';
 import * as schema from './schema';
 
 export function getDb() {
   // Grab the Cloudflare environment bindings injected by OpenNext
-  const { env } = getRequestContext();
+  const { env } = getCloudflareContext();
   
   // Return the initialized Drizzle client
   return drizzle(env.shoeworld_db, { schema });
